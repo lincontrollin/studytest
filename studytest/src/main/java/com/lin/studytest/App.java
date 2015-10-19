4b664a661bb44e0985e81b98939b80d8e671a3a8
Administrator <Administrator@ZLRSO24WZKCKDPO.mshome.net>
2015-10-20 00:32:53 +0800

    study test

---
+++ studytest/src/main/java/com/lin/studytest/App.java
@@ -0,0 +1,13 @@
+package com.lin.studytest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ studytest/src/main/java/com/lin/studytest/netty/main/TestMain.java
@@ -0,0 +1,12 @@
+package com.lin.studytest.netty.main;
+
+public class TestMain {
+	
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+		System.out.println(1);
+	}
+
+}
